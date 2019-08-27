a848bab8b816faf77decc9f15616f4447d3fcaaf
Ivan Krutov <vania-pooh@vania-pooh.com>
2019-08-27 20:34:15 +0300

    Automatically removing old generated style and script files

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ cd ${CLONE_DIR}
 git config user.name ${DRONE_COMMIT_AUTHOR}
 git config user.email ${DRONE_COMMIT_AUTHOR_EMAIL}
 git checkout gh-pages
+git rm ${CLONE_DIR}/main.*.css
+git rm ${CLONE_DIR}/main.*.js
 cp -R ${WD}/dist/* ${CLONE_DIR}
 git add --all
 git commit -am "Updated by ${DRONE_COMMIT_AUTHOR}"
